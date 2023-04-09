f0b2d8996a064f5cea08fa55fd8d39470745174a
sbui6 <bbui91829@gmail.com>
2023-04-09 18:33:25 -0400

    Fixed up combat scenes

--- Assets/Scripts/CombatScripts/enemymovementscripts/EnemyMoveLeft.cs
+++ Assets/Scripts/CombatScripts/enemymovementscripts/EnemyMoveLeft.cs
@@ -9,7 +9,7 @@ public class EnemyMoveLeft : StateMachineBehaviour
     
     EnemyStats enemy;
     Transform placeToGo;
-    public float speed = 1f;   
+    public float speed = 1f;
 
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
