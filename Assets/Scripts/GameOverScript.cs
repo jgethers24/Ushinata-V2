4fced709b311ccedd415e9858b14cf8ae0d33a87
sbui6 <bbui91829@gmail.com>
2023-04-16 03:16:00 -0400

    Made GameOver screen

---
+++ Assets/Scripts/GameOverScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameOverScript : MonoBehaviour
+{
+    CharacterStats health;
+
+    public void RestartButton()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void ExitButton()
+    {
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
